6d5eea20099f8d0e991803df3254eac1d7c691ea
Marc Duby <mduby@broadinstitute.org>
2026-03-31 10:05:34 -0400

    gpu: added gpu indentification script

--- src/python/verify_gpu.py
+++ src/python/verify_gpu.py
@@ -5,4 +5,11 @@ print("is there GPU: {}".format(torch.cuda.is_available()))
 print("number of GPUs: {}".format(torch.cuda.device_count()))
 print("name of GPU: {}".format(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "no-gpu"))
 
+print()
+
+print("torch:", torch.__version__)
+print("cuda available:", torch.cuda.is_available())
+print("gpu count:", torch.cuda.device_count())
+print("gpu name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "no-gpu")
+
 
