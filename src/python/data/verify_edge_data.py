639b7fecbb8b0720dc2dffb3237dde70290db1ac
Marc Duby <mduby@broadinstitute.org>
2026-04-13 12:48:13 -0400

    train: troubleshotting train data error

--- src/python/data/verify_edge_data.py
+++ src/python/data/verify_edge_data.py
@@ -15,3 +15,13 @@ print("sample patient phenotypes:", vals[:10])
 print("mapped", sum(v in hpo for v in vals), "of", len(vals))
 
 
+root = Path("/home/ubuntu/Data/Sheperd/HuAndPigeanHpData")
+p = root / "knowledge_graph/8.9.21_kg/hpo_to_idx_dict_8.9.21_kg.pkl"
+
+with open(p, "rb") as f:
+    hpo = pickle.load(f)
+
+keys = list(hpo.keys())[:20]
+print(keys)
+print("hp-style keys:", sum(str(k).startswith("HP:") for k in keys), "of", len(keys))
+
